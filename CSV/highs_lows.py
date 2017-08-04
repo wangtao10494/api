df3f1eee7ddf89b4e300094962adc57c4188a092
wangtao <313686598@qq.com>
2017-08-04 17:45:11 +0800

    8/4

---
+++ CSV/highs_lows.py
@@ -0,0 +1,27 @@
+import csv
+from matplotlib import pyplot as plt
+
+#从文件中获取高温
+filename = 'sitka_weather_07-2014.csv'
+with open(filename)as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+	
+	highs = []
+	for row in reader:
+		high = int(row[1])
+		highs.append(high)
+		
+#根据数据绘制图形
+fig = plt.figure(dpi=128,figsize=(10,6))
+plt.plot(highs,c='red')
+
+#设置图形的格式
+plt.title('日常最高气温,2014年7月')
+plt.xlabel('',fontsize=16)
+plt.ylabel('Temperature(F)',fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=16)
+
+plt.show()
+
+	
